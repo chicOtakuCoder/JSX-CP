474e6172885e2b3b661e472f60bb55b606d45e0d
Yvonne Irenroa <yvonneirenroa@gmail.com>
2022-01-03 10:45:12 +0100

    JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import image from './imageInSrc.jpg';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border: "solid 1px black", maxWidth: "100vw"}}>
+        <h1 className="title red">Yvonne Irenroa</h1>
+        <div>
+          <img src= {image} alt= "HNY 2022" />
+        </div>
+        <div>
+          <img src="/imageInPublic.png" alt= "HNY 2022 white" />
+        </div>
+      </div>
+        <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+        </video>
     </div>
   );
 }
